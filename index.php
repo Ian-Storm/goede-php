47e3ba583292bcac32bc86f58b73e06509f2924b
Ian Storm <99039737@mydavinci.nl>
2017-05-09 13:35:52 +0200

    update

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+
+date_default_timezone_set("Europe/Amsterdam"); 
+$time = "19:01";
+
+?>
+
+<html>
+<head>
+<title>Goede morgen</title>
+<link rel="stylesheet" type="text/css" href="goede.css">
+</head>
+<body>
+
+<?php
+if ($time >= "06:00" && $time < "12:00") {
+	echo "<h1>Good morning</h1><p>The time is $time.</p>";
+	echo "<style> body {background-image:url(backgrounds/morning.png); background-size: cover;} </style>";
+} elseif ($time >= "12:00" && $time < "18:00") {
+	echo "<h1>Good afternoon</h1><p>The time is $time.</p>";
+	echo "<style> body {background-image:url(backgrounds/afternoon.png); background-size: cover;} </style>";
+} elseif ($time >= "18:00" && $time < "24:00") {
+	echo "<h1>Good evening</h1><p>The time is $time.</p>";
+	echo "<style> body {background-image:url(backgrounds/evening.png); background-size: cover;} </style>";
+} elseif ($time >= "00:00" && $time < "06:00") {
+	echo "<h1>Good night</h1><p>The time is $time.</p>";
+	echo "<style> body {background-image:url(backgrounds/night.png); background-size: cover;} </style>";
+}
+?>
+
+</body>
+</html>

89998a20fca67809a4de734d6bd2a06f7e32fde5
Ian Storm <99039737@mydavinci.nl>
2017-06-06 11:30:40 +0200

    update

--- index.php
+++ index.php
@@ -9,9 +9,18 @@ $time = date("H:i:s");
 <head>
 <title>Goede morgen</title>
 <link rel="stylesheet" type="text/css" href="goede.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 </head>
 <body>
-
+<script type="text/javascript">
+var timeout = setTimeout(reload, 1000);
+    function reload(){
+        $("#reload").load('index.php #reload', function(){
+            timeout = setTimeout(reload, 1000);
+        });
+    }
+        </script>
+<div id="reload">
 <?php
 if ($time >= "06:00" && $time < "12:00") {
 	echo "<h1>Good morning</h1><p>The time is $time.</p>";
@@ -27,6 +36,6 @@ if ($time >= "06:00" && $time < "12:00") {
 	echo "<style> body {background-image:url(backgrounds/night.png); background-size: cover;} </style>";
 }
 ?>
-
+</div>
 </body>
 </html>
